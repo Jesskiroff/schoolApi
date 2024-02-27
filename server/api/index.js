ffd4b92f6e9a44593b99cbe87cb7ed4401658ed3
Jessica Kiroff <jesskiroff@gmail.com>
2024-02-27 16:28:21 -0500

    hook up db and express server properly

--- server/api/index.js
+++ server/api/index.js
@@ -2,4 +2,4 @@ const router = require('express').Router();
 module.exports = router;
 
 //Route: /api
-router.use('/students', require('./students))
+//router.use('/students', require('./students))
--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,28 @@
 const pg = require('pg')
 const fs = require('fs')
+require('dotenv').config()
 
-// const client = new pg.Client('postgres://localhost/school')
+const pool = new pg.Pool ({
+    user: process.env.PG_USERNAME,
+    host: 'localhost',
+    database: 'school',
+    password: process.env.PG_PASSWORD,
+    port: 5432,
+});
+
+const sql = fs.readFileSync('seed.sql').toString();
+
+async function seed(){
+    try{
+        await pool.connect()
+        await pool.query(sql);
+        console.log('Seeding completely successfully');
+    }   catch (err) {
+        console.error(err);
+    }   finally {
+       // await pool.end();
+    }
+}
+
+seed();
+module.exports = { pool };
